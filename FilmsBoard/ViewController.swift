1a8b3004993dab0ceb8559b53fabe1c8e2a1f30b
Pablo Sanchez Egido <pablosze@gmail.com>
2018-03-07 18:14:29 +0100

    Initial commit

---
+++ FilmsBoard/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  FilmsBoard
+//
+//  Created by Pablo on 07/03/2018.
+//  Copyright © 2018 Pablo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
